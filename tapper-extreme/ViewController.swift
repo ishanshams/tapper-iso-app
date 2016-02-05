61e1e9695471d73b2afba7c94528f7a65d2ff5b1
Ishan Shams <ishanshams@Ishans-MBP.home>
2016-02-05 18:40:43 -0500

    Complete IOS Project Tapper

--- tapper-extreme/ViewController.swift
+++ tapper-extreme/ViewController.swift
@@ -10,16 +10,75 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    //variables
+    var maxTaps : Int = 0
+    var currentTaps : Int = 0
+    
+    
+    //outlets
+    @IBOutlet weak var logoImg : UIImageView!
+    @IBOutlet weak var howManyTapsTxt : UITextField!
+    @IBOutlet weak var playBtn : UIButton!
+    
+    @IBOutlet weak var tapBtn : UIButton!
+    @IBOutlet weak var tapsLbs : UILabel!
+    
+    
+    @IBAction func onCoinTapped (sender : UIButton!) {
+        currentTaps++
+        updateTapsLabel()
+        
+        if isGameOver() {
+            restartGame()
+        }
+    }
+    
+    @IBAction func onPlayBtnPressed (sender: UIButton!) {
+        
+    
+        if howManyTapsTxt.text != nil && howManyTapsTxt.text != "" {
+            logoImg.hidden = true;
+            playBtn.hidden = true;
+            howManyTapsTxt.hidden = true;
+            
+            
+            tapBtn.hidden = false;
+            tapsLbs.hidden = false;
+            
+            
+            maxTaps = Int(howManyTapsTxt.text!)!
+            currentTaps = 0
+            
+            updateTapsLabel()
+            
+        }
+    }
+    
+    func restartGame () {
+        maxTaps = 0
+        howManyTapsTxt.text = ""
+        
+        logoImg.hidden = false;
+        playBtn.hidden = false;
+        howManyTapsTxt.hidden = false;
+        
+        
+        tapBtn.hidden = true;
+        tapsLbs.hidden = true;
+        
+    }
+    
+    func isGameOver() -> Bool {
+        if currentTaps >= maxTaps {
+            return true
+        }
+        else {
+            return false
+        }
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func updateTapsLabel() {
+        tapsLbs.text = "\(currentTaps) Taps"
     }
-
-
 }
 
